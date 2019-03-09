a465b217d1e8fb4cf12a31d64aed727179b2c3fb
zr <Wll001992Yx@163.com>
2019-03-09 22:16:19 +0800

    temp

--- perf.js
+++ perf.js
@@ -5,24 +5,14 @@ const dataDesc = (target, prop, value) => {
   }
 };
 // 监听事件
-dataDesc(EventTarget.prototype, 'on', function(type, callback) {
-  this.addEventListener(type, callback);
-});
+dataDesc(EventTarget.prototype, 'on', EventTarget.prototype.addEventListener);
 // 移除事件
-dataDesc(EventTarget.prototype, 'off', function(type, callback) {
-  this.removeEventListener(type, callback);
-});
+dataDesc(EventTarget.prototype, 'off', EventTarget.prototype.removeEventListener);
 // 触发事件
-dataDesc(EventTarget.prototype, 'fire', function(type, detail, options) {
-  if (detail || options) {
-    options = Object.assign({
-      bubbles: false,
-      cancelable: true,
-    }, options, {
-      detail,
-    });
-    return this.dispatchEvent(new CustomEvent(type, options));
-  } else {
-    return this.dispatchEvent(new Event(type));
-  }
+dataDesc(EventTarget.prototype, 'fire', function(type, detail = Object.create(null)) {
+  this.dispatchEvent(new CustomEvent(type, {
+    bubbles: false,
+    cancelable: false,
+    detail,
+  }));
 });
